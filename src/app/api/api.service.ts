229c1c8fa4350644965c6e652186c3f4d497ca2f
Matej Šmitala <matej.smitala@gmail.com>
2017-03-30 23:12:38 +0200

    env config

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -3,9 +3,7 @@ import 'rxjs/Rx';
 import { RestResource } from './rest-resource';
 import { RestResourceFactory } from './rest-resource-factory';
 import { Bill, Trip } from './api-model';
-
-// const API_URL = 'http://cestak-api.herokuapp.com/api';
-const API_URL = 'http://localhost:8080/api';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ApiService {
@@ -13,7 +11,7 @@ export class ApiService {
   public trips: RestResource<Trip>;
 
   constructor(private restResourceFactory: RestResourceFactory) {
-    restResourceFactory.setApiUrl(API_URL);
+    restResourceFactory.setApiUrl(environment.apiUrl);
     this.bills = restResourceFactory.createResource('/bill');
     this.trips = restResourceFactory.createResource('/trip');
   }
